33afa94f40ed59f2d6e34b102ec82784d5923bb9
Dunvant_kai <77610897+Dunvantkai@users.noreply.github.com>
2025-10-24 12:05:15 +1300

    Add Minecraft binary-to-command converter and assets
    Introduces batch scripts to convert binary instructions from TXT files into Minecraft commands, generating datapacks and mcfunction files. Adds sample data files, litematic schematic, and supporting metadata for Minecraft datapacks. Includes backup script and test assets for demonstration and development.

---
+++ 4096-BIT-Update/backup/Program.bat
@@ -0,0 +1,92 @@
+@echo off
+title Binary to Minecraft Program Code
+setlocal enabledelayedexpansion
+:RESTART
+CLS
+echo ================================
+echo Binary to Minecraft Program Code
+echo ================================
+echo.
+color 0a
+set /p TName=Enter TXT File Name (like "Test.txt"):
+if not exist "%TName%" (
+    echo File not found!
+    pause
+    Goto RESTART
+)
+for %%f in ("%TName%") do set "BaseName=%%~nf"
+mkdir %BaseName%
+set "Pack=MC_Data\pack.mcmeta"
+cd %BaseName%
+set "RendCom=%BaseName%_RendCom.txt" 
+copy "..\%TName%" "%RendCom%"
+for /f "usebackq delims=" %%A in ("%RendCom%") do (
+set "line=%%A"
+set "line=!line:*:=!"
+for /f "delims=:" %%B in ("%%A") do echo %%B >> "Temp.txt"
+)
+copy "%RendCom%" "%BaseName%.txt"
+copy "Temp.txt" "%RendCom%"
+del Temp.txt
+mkdir my_datapack
+copy "..\%Pack%" "my_datapack\pack.mcmeta"
+cd my_datapack
+mkdir data
+cd data
+mkdir myfunc
+cd myfunc
+mkdir function
+cd function
+echo say Start > runcommands.mcfunction"
+set "sot=setblock ~"
+set "squ= ~"
+set "glass= ~ minecraft:glass"
+set "red= ~ minecraft:redstone_block"
+set /a x=2
+set /a y=1
+for /f "usebackq delims=" %%A in ("..\..\..\..\%RendCom%") do call :process_line "%%A"
+goto END
+:process_line
+set "line=%~1"
+echo Line: !line!
+set /a pos=0
+:char_loop
+set "char=!line:~%pos%,1!"
+if not defined char goto :eof
+
+if "!char!"=="0" (
+    echo !sot!!x!!squ!!y!!glass! >> "runcommands.mcfunction"
+    set /a y=y + 2
+    set /a pos+=1
+    if "!y!"=="17" (
+    set /a x=x + 2
+    set /a y=1
+)
+    goto char_loop
+)
+if "!char!"=="1" (
+    echo !sot!!x!!squ!!y!!red! >> "runcommands.mcfunction"
+    set /a y=y + 2
+    set /a pos+=1
+    if "!y!"=="17" (
+    set /a x=x + 2
+    set /a y=1
+)
+    goto char_loop
+)
+set /a pos+=1
+goto char_loop
+
+:END
+echo say Finshed >> "runcommands.mcfunction"
+echo Done processing.
+cd..
+cd..
+cd..
+cd..
+cd..
+pause
+Goto RESTART
+
+
+
